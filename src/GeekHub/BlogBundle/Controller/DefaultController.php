3a6af03c788c25315915063bc66d682145cde29a
Bohdan Moshchenko <bohdan.moshchenko@gmail.com>
2016-11-27 14:45:59 +0200

    one parameter.

--- src/GeekHub/BlogBundle/Controller/DefaultController.php
+++ src/GeekHub/BlogBundle/Controller/DefaultController.php
@@ -6,8 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->render('GeekHubBlogBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @param $name
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function oneParameterAction($name)
+    {
+        return $this->render('GeekHubBlogBundle:Default:index.html.twig', [
+            'name' => $name,
+        ]);
     }
 }
